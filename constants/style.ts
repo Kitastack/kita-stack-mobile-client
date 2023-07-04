772e91d032e6ab77904ec6c6c25d9d1076d06768
agung-adhinata <adhi0asta@gmail.com>
2023-07-05 02:24:24 +0800

    feat: setup theme

---
+++ constants/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	flexRow: {
+		flex: 1,
+		flexDirection: "row",
+	},
+	flexCol: {
+		flex: 1,
+		flexDirection: "column",
+	},
+});
