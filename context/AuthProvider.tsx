f5bb921201bf6f9331c72432c0afb1cc83f8ce0d
Agung Adhinata <adhi0asta@gmail.com>
2023-07-13 03:02:46 +0800

    feat: refactor, fix logout stuck

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,92 +1,91 @@
 import { router, useSegments } from "expo-router";
 import React, {
-	useEffect,
-	createContext,
-	useContext,
-	useReducer,
-	useState,
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  useState,
 } from "react";
 import { $useStorage } from "@/hooks/useStorage";
 
 const intialState: any = null;
 
 export type IContext = {
-	state: typeof intialState;
-	dispatch: React.Dispatch<any>;
-	login?: (_val: any) => void;
-	logout?: () => void;
+  state: typeof intialState;
+  dispatch: React.Dispatch<any>;
+  login?: (_val: any) => void;
+  logout?: () => void;
 };
 
 export const AuthContext = createContext<IContext>({
-	state: intialState,
-	dispatch: () => null,
+  state: intialState,
+  dispatch: () => null,
 });
 
 export function useAuth() {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 export function authReducer(
-	state: any,
-	action: { type: string; payload?: any }
+  state: any,
+  action: { type: "login" | "logout"; payload?: any },
 ) {
-	switch (action.type) {
-		case "login": {
-			return action?.payload;
-		}
-		case "logout": {
-			state = null;
-			router.push("/sign_in");
-		}
-		// default: {
-		//   throw new Error(`Unhandled action type: ${action.type}`);
-		// }
-	}
+  switch (action.type) {
+    case "login": {
+      return action?.payload;
+    }
+    case "logout": {
+      state = null;
+      router.push("/sign_in");
+    }
+    // default: {
+    //   throw new Error(`Unhandled action type: ${action.type}`);
+    // }
+  }
 }
 
 export default function Provider(props: any) {
-	const [$isReady, $setReady] = useState(false);
-	const [state, dispatch] = useReducer(authReducer, intialState);
-	const segments = useSegments();
+  const [$isReady, $setReady] = useState(false);
+  const [state, dispatch] = useReducer(authReducer, intialState);
+  const segments = useSegments();
 
-	// mounted
-	useEffect(() => {
-		(async function _() {
-			const _userFromStorage = await $useStorage("user");
-			if (_userFromStorage?._id)
-				dispatch({ type: "login", payload: _userFromStorage });
-			router.push("/sign_in");
-			$setReady(true);
-		})();
-	}, []);
+  // mounted
+  useEffect(() => {
+    (async function _() {
+      const _userFromStorage = await $useStorage("user");
+      if (_userFromStorage?._id)
+        dispatch({ type: "login", payload: _userFromStorage });
+      // router.replace("/sign_in");
+      $setReady(true);
+    })();
+  }, []);
 
-	// updated based on mounted
-	useEffect(() => {
-		if ($isReady) {
-			const inAuthGroup = segments[0] === "(auth)";
-			const inTabsGroup = segments[0] === "(tabs)";
-			const inCustomGroup = segments[0] === "(custom)";
-			console.log("ready", $isReady, state?._id);
-			if (!state?._id && !inAuthGroup) {
-				router.push("/sign_in");
-			} else if (state?._id && inAuthGroup) {
-				router.push("/tolong");
-			}
+  // updated based on mounted
+  useEffect(() => {
+    if ($isReady) {
+      const inAuthGroup = segments[0] === "(auth)";
+      const inTabsGroup = segments[0] === "(tabs)";
+      const inCustomGroup = segments[0] === "(custom)";
+      const isUserLogged = state?._id;
 
-			// else if (state?._id && inAuthGroup) {
-			//   router.push("/tolong");
-			// }
-		}
-	}, [state, segments, $isReady]);
+      console.log("ready", $isReady, state?._id);
 
-	return (
-		<AuthContext.Provider
-			value={{
-				state,
-				dispatch,
-			}}
-		>
-			{props.children}
-		</AuthContext.Provider>
-	);
+      if (!isUserLogged && !inAuthGroup) {
+        router.replace("/sign_in");
+      } else if (isUserLogged && inAuthGroup) {
+        router.replace("/tolong");
+      }
+    }
+  }, [state, segments, $isReady]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
